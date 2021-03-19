7708f48612485fcb394dae32da2f8a813ca94ae7
Erika Leite <erika.l3it3@gmail.com>
2021-03-19 13:09:24 -0300

    interfaces finalizadas: genero e filme

--- senai_filmes_webApi/senai_filmes_webApi/Interfaces/IFilmeRepository.cs
+++ senai_filmes_webApi/senai_filmes_webApi/Interfaces/IFilmeRepository.cs
@@ -8,14 +8,23 @@ namespace senai_filmes_webApi.Interfaces
 {
     public interface IFilmeRepository
     {
-        //Nos retornará todos os generos listados
+        //Nos retornará todos os filmes listados
         List<FilmeDomain> ListarTodos();
 
-        //Nos retornará apenas um genero pelo id informado no (parametro)
+        //Nos retornará apenas um filme pelo id informado no (parametro)
         FilmeDomain BuscarPorId(int id);
 
-        //o método cadastrará um novo gênero, incluindo id e nome
+        //o método cadastrará um novo filme, incluindo id, titulo e o id do genero
         void Cadastrar(FilmeDomain novoFilme);
+
+        //O método atualizará um filme existente utilizando o id no corpo da request
+        void AtualizarIdCorpo(FilmeDomain filme);
+
+        //O método atualizará um filme através da url (ex.: localhost:5000/filme/2 - sendo "2" o id informado na request)
+        void AtualizarIdUrl(int id, FilmeDomain filme);
+
+        //o método deletará o filme cujo id seja referenciado no paramentro (ex.: o filme id: 1 titulo: X, será deletado quando o id "1" for inserido no paramentro do método)
+        void Deletar(int id);
 
     }
 }
--- senai_filmes_webApi/senai_filmes_webApi/Interfaces/IGeneroRepository.cs
+++ senai_filmes_webApi/senai_filmes_webApi/Interfaces/IGeneroRepository.cs
@@ -8,7 +8,7 @@ namespace senai_filmes_webApi.Interfaces
 {
    /// <summary>
    /// Classe responsável pela implementação de regras de negócio, o que é necessário possuir no projeto
-   /// neste caso, implementaremos o CRUD - base para criação do projeto
+   /// Neste caso, implementaremos o CRUD - base para criação do projeto
    /// </summary>
     public interface IGeneroRepository
     {
@@ -18,7 +18,18 @@ namespace senai_filmes_webApi.Interfaces
         //Nos retornará apenas um genero pelo id informado no (parametro)
         GeneroDomain BuscarPorId(int id);
 
-        //o método cadastrará um novo gênero, incluindo id e nome
+        //O método cadastrará um novo gênero, incluindo id e nome
         void Cadastrar(GeneroDomain novoGenero);
+
+        //O método atualizará um genero existente utilizando o id no corpo da request
+        void AtualizarIdCorpo(GeneroDomain genero);
+
+        //O método atualizará um genero através da url (ex.: localhost:5000/genero/4 - sendo "4" o id informado na request)
+        void AtualizarIdUrl(int id, GeneroDomain genero);
+
+        //o método deletará o genero cujo id seja referenciado no paramentro (ex.: o genero id: 1 nome: X, será deletado quando o id "1" for inserido no paramentro do método)
+        void Deletar(int id);
+
+
     }
 }
